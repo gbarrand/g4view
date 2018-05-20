9c60e6ff64ea84ba8570600f67d45c8b19595ea5
Guy Barrand <guy.barrand@gmail.com>
2018-05-20 16:35:59 +0200

    update

---
+++ g4view/Windows/main.cpp
@@ -0,0 +1,13 @@
+// Copyright (C) 2010, Guy Barrand. All rights reserved.
+// See the file g4view.license for terms.
+
+#include "../g4view/main"
+
+#include <exlib/app/Windows/main_cpp>
+
+int main(int argc,char** argv) {return exlib_main<g4view::context,g4view::main>("g4view",argc,argv);}
+
+//exlib_build_use inlib expat
+//exlib_build_use exlib png jpeg zlib inlib_glutess freetype
+//exlib_build_use GL Windows kernel
+
