311021b86bbc422ac41ba7591702906a5559d7ee
Guy Barrand <guy.barrand@gmail.com>
2021-08-27 12:18:49 +0200

    update

---
+++ g4view/wasm/main.cpp
@@ -0,0 +1,13 @@
+// Copyright (C) 2010, Guy Barrand. All rights reserved.
+// See the file g4view.license for terms.
+
+#include "../g4view/main"
+
+#define APP_NAME g4view
+
+#include <exlib/app/wasm/main_cpp>
+
+//exlib_build_use inlib expat
+//exlib_build_use exlib png jpeg zlib inlib_glutess freetype kernel
+
+//exlib__build_use skip
